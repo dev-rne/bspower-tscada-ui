5d7d0163c90f1111c796937845f5b7035ff58c35
coco <coco.wapplab@gmail.com>
2022-03-24 15:51:42 +0900

    🐛 merge...

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sub03 from "./features/sub03";
+import main from "./features/main";
 
 export default configureStore({
     reducer: {
         sub03: sub03,
+        main
     },
 });
