9ec6c27c11a1d8d60e7ab921a2532f0ea2355b37
choichanho <chanho@wapplab.com>
2022-03-25 14:26:04 +0900

    [ROGAN] cctv redux 추가

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import occ from "./features/Occ";
+import occ from "./features/occ";
 import main from "./features/main";
+import cctv from "./features/cctv";
 
 export default configureStore({
     reducer: {
         occ,
-        main
+        main,
+        cctv,
     },
 });
